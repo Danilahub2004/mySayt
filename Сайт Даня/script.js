00fe5165906b5653084e672e3a38251118e980f2
Danilahub2004 <137624609+Danilahub2004@users.noreply.github.com>
2024-10-24 21:20:03 +0300

    Delete Сайт Даня directory

--- Сайт Даня/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const articles = document.querySelectorAll('.article');
-
-    articles.forEach((article) => {
-        article.addEventListener('click', () => {
-            alert(`Вы кликнули на статью: ${article.querySelector('.article__title').textContent}`);
-        });
-    });
-});
